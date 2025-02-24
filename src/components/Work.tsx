9fff4e3a4e2d441f0fddcadc54d54a9f0c826cac
Syosuke Hasegawa <syosukecreator@gmail.com>
2025-02-24 18:40:49 +0900

    内容を追加

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -2,7 +2,54 @@ const Work = () => {
   return (
     <div>
       <h2>Work</h2>
-      <p>仕事の内容</p>
+      <table>
+        <tr>
+          <th>項目</th>
+          <th>アサイン期間</th>
+          <th>使用した技術</th>
+        </tr>
+        <tr>
+          <td>NFTゲーム</td>
+          <td>2022/08〜2023/11</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>RPGゲーム</td>
+          <td>2020/03〜2021/07</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>メタバースショッピングモールアプリ</td>
+          <td>2021/07〜2022/03</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>リズムアクションRPGゲーム</td>
+          <td>2018/02〜2020/03</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>タワーディフェンスゲーム</td>
+          <td>2016/04〜2018/01</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>電⼒会社⽤Webアプリ</td>
+          <td>2014/11〜2015/08</td>
+          <td>Eclipse・Java</td>
+        </tr>
+        <tr>
+          <td>美少女育成ゲーム</td>
+          <td>2014/07〜2014/11</td>
+          <td>Unity・C#</td>
+        </tr>
+        <tr>
+          <td>スノーボードゲーム</td>
+          <td>2014/04〜2014/06</td>
+          <td>Unity・C#・Android Studio</td>
+        </tr>
+      </table>
+      <p>※上に行くほど新しい</p>
     </div>
   );
 };
